ff7dbbe3bc05b09f7a720ba978070794e7ff4d3a
“LittleBell123 <415725257@qq.com>
2019-01-26 18:09:40 +0800

    把order 做的事情让order做

--- src/main/java/org/katas/refactoring/Order.java
+++ src/main/java/org/katas/refactoring/Order.java
@@ -3,25 +3,57 @@ package org.katas.refactoring;
 import java.util.List;
 
 public class Order {
-    String name;
-    String address;
+    private static final double taxRate = .10;
+	String customerName;
+    String customerAddress;
     List<LineItem> lineItems;
 
-    public Order(String name, String address, List<LineItem> lineItems) {
-        this.name = name;
-        this.address = address;
+    public Order(String customerName, String customerAddress, List<LineItem> lineItems) {
+        this.customerName = customerName;
+        this.customerAddress = customerAddress;
         this.lineItems = lineItems;
     }
 
     public String getCustomerName() {
-        return name;
+        return customerName;
     }
 
     public String getCustomerAddress() {
-        return address;
+        return customerAddress;
     }
 
     public List<LineItem> getLineItems() {
         return lineItems;
     }
+
+	public String receipt() {
+		StringBuilder output = new StringBuilder();
+		output.append("======Printing Orders======\n");
+
+        output.append(getCustomerName());
+        output.append(getCustomerAddress());
+
+		double totalSalesTax = 0d;
+		double total = 0d;
+		for (LineItem lineItem : getLineItems()) {
+			output.append(lineItem.getDescription());
+			output.append('\t');
+			output.append(lineItem.getPrice());
+			output.append('\t');
+			output.append(lineItem.getQuantity());
+			output.append('\t');
+			output.append(lineItem.totalAmount());
+			output.append('\n');
+
+            double salesTax = lineItem.totalAmount() * taxRate;
+            totalSalesTax += salesTax;
+
+            total += lineItem.totalAmount() + salesTax;
+		}
+
+		output.append("Sales Tax").append('\t').append(totalSalesTax);
+
+		output.append("Total Amount").append('\t').append(total);
+		return output.toString();
+	}
 }
--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -8,7 +8,6 @@ package org.katas.refactoring;
  * 
  */
 public class OrderReceipt {
-    private static final double taxRate = .10;
 	private Order order;
 
     public OrderReceipt(Order order) {
@@ -16,34 +15,6 @@ public class OrderReceipt {
 	}
 
 	public String printReceipt() {
-		StringBuilder output = new StringBuilder();
-
-		output.append("======Printing Orders======\n");
-
-        output.append(order.getCustomerName());
-        output.append(order.getCustomerAddress());
-
-		double totalSalesTax = 0d;
-		double total = 0d;
-		for (LineItem lineItem : order.getLineItems()) {
-			output.append(lineItem.getDescription());
-			output.append('\t');
-			output.append(lineItem.getPrice());
-			output.append('\t');
-			output.append(lineItem.getQuantity());
-			output.append('\t');
-			output.append(lineItem.totalAmount());
-			output.append('\n');
-
-            double salesTax = lineItem.totalAmount() * taxRate;
-            totalSalesTax += salesTax;
-
-            total += lineItem.totalAmount() + salesTax;
-		}
-
-		output.append("Sales Tax").append('\t').append(totalSalesTax);
-
-		output.append("Total Amount").append('\t').append(total);
-		return output.toString();
+		return order.receipt();
 	}
 }
