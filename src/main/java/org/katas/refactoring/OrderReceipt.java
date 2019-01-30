06b4be7f7a693c92e05fecf703da1a57e8fdb6c7
LittleBell123 <415725257@qq.com>
2019-01-30 14:57:48 +0800

    试一试能不能push

--- src/main/java/org/katas/refactoring/OrderReceipt.java
+++ src/main/java/org/katas/refactoring/OrderReceipt.java
@@ -5,7 +5,7 @@ package org.katas.refactoring;
  * price and amount. It also calculates the sales tax @ 10% and prints as part
  * of order. It computes the total order amount (amount of individual lineItems +
  * total sales tax) and prints it.
- * 
+ * 试一下能不能push
  */
 public class OrderReceipt {
 	private Order order;
